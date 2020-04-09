3a29a36aa729e0caf2d689f404260f625f00ff99
Bolaji Olajide <cooproton@gmail.com>
2020-04-09 03:41:19 +0100

    add utility method for paginating

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as generateCSV } from './generate-csv';
 export { default as generateRandomColor } from './generate-random-color';
 export { default as generateShortCode } from './generate-short-code';
+export { default as paginate } from './paginate';
---
+++ src/paginate.ts
@@ -0,0 +1,16 @@
+/**
+ * An attempt at manual pagination
+ *
+ * @param {number}  limit   the max amount of items in the slice
+ * @param {number}  page    the page to retrieve
+ * @param {Array}   data    the list of records to be paginated
+ *
+ * @returns {Array}        a slice of the array containing fewer records
+ */
+const paginate = (limit: number, page: number, data: [any]) => {
+  const offset = (page - 1) * limit;
+  const newLimit = offset + limit;
+  return data.slice(offset, newLimit);
+};
+
+export default paginate;
