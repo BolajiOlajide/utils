cac8fc01039f291be5380a6ddfcdaae0ef03da04
Bolaji Olajide <cooproton@gmail.com>
2020-04-09 14:29:39 +0100

    add list of countries

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as countries } from './countries';
 export { default as generateCSV } from './generate-csv';
 export { default as generateRandomColor } from './generate-random-color';
 export { default as generateShortCode } from './generate-short-code';
