de6367a36e8e40335be4ad7405b653879554b647
Bolaji Olajide <cooproton@gmail.com>
2021-01-04 22:04:14 +0100

    1.0.14

--- __tests__/is-http-url.spec.ts
+++ __tests__/is-http-url.spec.ts
@@ -1,24 +1,24 @@
-import { isUrl } from '../src';
+import { isHttpUrl } from '../src';
 
 
 describe('isUrl', (): void => {
   test('returns "true" if argument is a valid HTTP URL', (): void => {
-    const result: boolean = isUrl('https://anniemusic.app');
+    const result: boolean = isHttpUrl('https://anniemusic.app');
     expect(result).toBe(true);
   });
 
   test('returns "false" if argument isn\'t a HTTP url', (): void => {
-    const result: boolean = isUrl('spotify:track:1YLJVmuzeM2YSUkCCaTNUB');
+    const result: boolean = isHttpUrl('spotify:track:1YLJVmuzeM2YSUkCCaTNUB');
     expect(result).toBe(false);
   });
 
   test('returns "false" if argument is an invalid URL', (): void => {
-    const result: boolean = isUrl('https://a');
+    const result: boolean = isHttpUrl('https://a');
     expect(result).toBe(false);
   });
 
   test('returns "false" if argument is a normal string', (): void => {
-    const result: boolean = isUrl('proton');
+    const result: boolean = isHttpUrl('proton');
     expect(result).toBe(false);
   });
 });
