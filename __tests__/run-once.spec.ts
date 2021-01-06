e9b7e04afe11fe867024b2ef2d5f8def250d71ae
Bolaji Olajide <cooproton@gmail.com>
2021-01-06 18:43:22 +0100

    chore: fix lint issues with test

--- __tests__/run-once.spec.ts
+++ __tests__/run-once.spec.ts
@@ -1,19 +1,24 @@
-import { runOnce } from "../src";
+import { runOnce } from '../src';
 
 describe('runOnce', (): void => {
   test('returns a function', (): void => {
-    const result = runOnce(()=> console.log('runOnce'))
-    expect(typeof result).toEqual('function')
+    // eslint-disable-next-line no-console
+    const result = runOnce(() => console.log('runOnce'));
+
+    expect(typeof result).toEqual('function');
   });
 
   test('returns a function that runs only once', (): void => {
-    const addTogether = function(num1:number, num2:number):number{
-      return num1+num2
-    }
-    const addTogetherOnce = runOnce(addTogether)
-    let firstResult = addTogetherOnce(0,1)
-    let secondResult = addTogetherOnce(0,1)
-    expect(firstResult).toEqual(1)
-    expect(secondResult).toBe(undefined)
+    const addTogether = function (num1: number, num2: number): number {
+      return num1 + num2;
+    };
+
+    const addTogetherOnce = runOnce(addTogether);
+
+    const firstResult = addTogetherOnce(0, 1);
+    const secondResult = addTogetherOnce(0, 1);
+
+    expect(firstResult).toEqual(1);
+    expect(secondResult).toBe(undefined);
   });
 });
